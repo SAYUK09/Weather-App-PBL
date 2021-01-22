88fabe0add08059d6dfe700a404d76ac6b97b81d
SAYUK09 <sayurikamble123@gmail.com>
2021-01-22 13:36:21 +0530

    Async function

--- script.js
+++ script.js
@@ -1,40 +1,32 @@
+    // const url = "https://api.openweathermap.org/data/2.5/weather";
+    // const API_KEY = "a8e71c9932b20c4ceb0aed183e6a83bb"
+    
 
-
-
-const API_KEY = "a8e71c9932b20c4ceb0aed183e6a83bb"
-
-function fetchURL (){
+const asyncFetch = async () =>{
     const city = document.getElementById('city-input').value;
-    const URL = "https://api.openweathermap.org/data/2.5/weather";
-    const fullURL = `${URL}?q=${city}&appid=${API_KEY}&units=imperial`
-    console.log(fullURL)
-    console.log(city)
-
-    return fetch(fullURL)
-    .then((response)=>response.json())
-    .then((json)=>{
-        
-        
-        // console.log(json)
-        console.log ("yeaahhhh")
+    const fullURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a8e71c9932b20c4ceb0aed183e6a83bb&units=imperial` 
     
-        // document.getElementById("city-name").innerText = json.name;
-        // document.getElementById("weather-type").innerText = json.weather[0].main;
-       
-
-        // return json
-        data (json)
-        }
-    )
+    const response = await fetch(fullURL);
+    const data = await response.json();
+    // data(json);
+    return data 
     
+}
+
+function fetchURL () {
+
+    asyncFetch()
+    .then((data) => { info (data)})
+}
+function info (json){
    
-    
+    document.getElementById("city-name").innerText = json.name;
+    document.getElementById("weather-type").innerText = json.weather[0].main;
+    document.getElementById("temp").innerText =json.main.temp;
+    document.getElementById("min-temp").innerText =json.main.temp_min;
+    document.getElementById("max-temp").innerText = json.main.temp_max;
+
 }
 
 
-
-function data (json){
-    // document.getElementById("city-name").innerText = json.name;
-    console.log(json)
-}
 
